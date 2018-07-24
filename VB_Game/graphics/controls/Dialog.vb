b58f1c818f8aae08cff15302d2c2e4fef778e483
Eric Holmstrom <eric.holmstrom.sydney@gmail.com>
2018-07-24 11:10:32 +1000

    working on adding tutorial screen and dialog

---
+++ VB_Game/graphics/controls/Dialog.vb
@@ -0,0 +1,27 @@
+﻿Public Class Dialog : Inherits Control
+
+    Private okBtn As Button
+    Private cancelBtn As Button
+    Private dialogDefaultSize As New OpenTK.Vector2(300 * Constants.DESIGN_SCALE_FACTOR, 200 * Constants.DESIGN_SCALE_FACTOR)
+    Private dialogBackColour = Drawing.Color.FromArgb(255, 60, 60, 60)
+    'Darkens everything behind dialog
+    Private fullscreenTint As New ShapeTexture(Constants.DESIGN_WIDTH, Constants.DESIGN_WIDTH,
+                                               Drawing.Color.FromArgb(128, 0, 0, 0), ShapeTexture.ShapeType.Rectangle)
+    Private btnStyle As New ButtonStyle(Drawing.Brushes.White, Drawing.Color.FromArgb(255, 20, 20, 20))
+    Private btnSize = New Drawing.Size(80 * Constants.DESIGN_SCALE_FACTOR, 40 * Constants.DESIGN_SCALE_FACTOR)
+
+    Public Sub New()
+        Me.customRender = True
+        Me.texture = New ShapeTexture(dialogDefaultSize.X, dialogDefaultSize.Y, dialogBackColour, ShapeTexture.ShapeType.Rectangle)
+        Me.pos = New OpenTK.Vector2(-dialogDefaultSize.X / 2, -dialogDefaultSize.Y / 2)
+        okBtn = New Button("OK", New OpenTK.Vector2(0), btnSize, btnStyle)
+    End Sub
+
+    Public Overrides Sub render(delta As Double)
+        MyBase.render(delta)
+        SpriteBatch.drawTexture(fullscreenTint, Constants.TOP_LEFT_COORD)
+        SpriteBatch.drawControl(Me)
+        okBtn.render(delta)
+    End Sub
+
+End Class
--- VB_Game/screens/overlays/TutorialScreenOverlay.vb
+++ VB_Game/screens/overlays/TutorialScreenOverlay.vb
@@ -3,26 +3,51 @@
 ''' </summary>
 Public Class TutorialScreenOverlay
 
-    Private enemyLabel As TextLabel
+    Private labels(4) As TextLabel
     Private mainFont As New Drawing.Font("Impact", 18 * Constants.DESIGN_SCALE_FACTOR, Drawing.FontStyle.Regular)
     Private msgBackgroundColor = Drawing.Color.FromArgb(80, 0, 0, 0)
-    Private msgBackgrounds(2) As ShapeTexture
-    Private msgBackgroundsPos(2) As OpenTK.Vector2
+    Private msgBackgrounds(3) As ShapeTexture
+    Private msgBackgroundsPos(3) As OpenTK.Vector2
     Private msgPadding = 10
 
     Public Sub New()
-        enemyLabel = New TextLabel("This is an enemy, \r\n avoid touching it", mainFont, Drawing.Brushes.White)
-        enemyLabel.pos = New OpenTK.Vector2(0, 0)
-        msgBackgrounds(0) = New ShapeTexture(enemyLabel.getWidth() + msgPadding * 2, enemyLabel.getHeight() + msgPadding * 2,
+        labels(0) = New TextLabel("This is an enemy", mainFont, Drawing.Brushes.White)
+        labels(0).pos = New OpenTK.Vector2(-labels(0).getWidth() / 2, -250 * Constants.DESIGN_SCALE_FACTOR)
+
+        labels(1) = New TextLabel("avoid touching it", mainFont, Drawing.Brushes.White)
+        labels(1).pos = New OpenTK.Vector2(labels(0).pos.X, labels(0).pos.Y + labels(0).getHeight())
+
+        msgBackgrounds(0) = New ShapeTexture(Math.Max(labels(0).getWidth(), labels(1).getWidth()) + msgPadding * 2, labels(0).getHeight() + labels(1).getHeight() + msgPadding * 2,
                                              msgBackgroundColor, ShapeTexture.ShapeType.Rectangle)
-        msgBackgroundsPos(0) = New OpenTK.Vector2(enemyLabel.pos.X - msgPadding, enemyLabel.pos.Y - msgPadding)
+        msgBackgroundsPos(0) = New OpenTK.Vector2(labels(0).pos.X - msgPadding, labels(0).pos.Y - msgPadding)
+
+        labels(2) = New TextLabel("Move using WASD", mainFont, Drawing.Brushes.White)
+        labels(2).pos = New OpenTK.Vector2(-labels(2).getWidth() / 2 + 16 * Constants.DESIGN_SCALE_FACTOR, 20 * Constants.DESIGN_SCALE_FACTOR)
+
+        msgBackgrounds(1) = New ShapeTexture(labels(2).getWidth() + msgPadding * 2, labels(2).getHeight() + msgPadding * 2,
+                                             msgBackgroundColor, ShapeTexture.ShapeType.Rectangle)
+        msgBackgroundsPos(1) = New OpenTK.Vector2(labels(2).pos.X - msgPadding, labels(2).pos.Y - msgPadding)
+
+        'Chest message
+        labels(3) = New TextLabel("This is a chest", mainFont, Drawing.Brushes.White)
+        labels(3).pos = New OpenTK.Vector2(135 * Constants.DESIGN_SCALE_FACTOR, -130 * Constants.DESIGN_SCALE_FACTOR)
+
+        labels(4) = New TextLabel("collect them", mainFont, Drawing.Brushes.White)
+        labels(4).pos = New OpenTK.Vector2(labels(3).pos.X, labels(3).pos.Y + labels(3).getHeight())
+
+        msgBackgrounds(2) = New ShapeTexture(Math.Max(labels(3).getWidth(), labels(4).getWidth()) + msgPadding * 2, labels(3).getHeight() + labels(4).getHeight() + msgPadding * 2,
+                                             msgBackgroundColor, ShapeTexture.ShapeType.Rectangle)
+        msgBackgroundsPos(2) = New OpenTK.Vector2(labels(3).pos.X - msgPadding, labels(3).pos.Y - msgPadding)
+
     End Sub
 
     Public Sub render(delta)
-        For i = 0 To 0
+        For i = 0 To 2
             SpriteBatch.drawTexture(msgBackgrounds(i), msgBackgroundsPos(i))
         Next
-        enemyLabel.render(delta)
+        For i = 0 To 4
+            labels(i).render(delta)
+        Next
     End Sub
 
 End Class
